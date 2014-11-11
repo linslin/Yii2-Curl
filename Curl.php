bfed4c39f33c5342b75fecc23022dc58939760ca
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-11-11 14:35:19 +0100

    Added default class structure

--- Curl.php
+++ Curl.php
@@ -19,4 +19,47 @@ namespace linslin\yii2\curl;
  */
 class Curl extends \yii\base\Widget
 {
+
+    // ################################################ class vars // ################################################
+
+    /**
+     * @var float timeout for request
+     * This value will be mapped to native curl `CURLOPT_CONNECTTIMEOUT` option.
+     */
+    public $connectionTimeout = null;
+
+
+    /**
+     * @var float timeout
+     * This value will be mapped to curl `CURLOPT_TIMEOUT` option.
+     */
+    public $dataTimeout = null;
+
+
+    /**
+     * @var string
+     * Holds response data right after sending a request.
+     */
+    public $response = null;
+
+
+    /**
+     * @var integer HTTP-Status Code
+     * This value will be hold HTTP-Status Code. False if request was not successfull.
+     */
+    public $responseCode = null;
+
+
+
+    // ############################################### class methods // ##############################################
+
+    /**
+     * Init function
+     *
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+    }
 }
