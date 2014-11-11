1f4ba828cb317cd7afa577beb4c571238caabdd2
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-11-11 15:41:27 +0100

    RC Release

--- Curl.php
+++ Curl.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Yii2 cURL wrapper
+ * With RESTful support. Inspired by wenbin1989/yii2-curl.
  *
  * @category  Web-yii2
  * @package   yii2-curl
@@ -13,6 +14,12 @@
  */
 
 namespace linslin\yii2\curl;
+
+use Yii;
+use yii\base\Component;
+use yii\base\Exception;
+use yii\helpers\Json;
+use yii\web\HttpException;
 
 /**
  * cURL class
@@ -45,9 +52,23 @@ class Curl extends \yii\base\Widget
 
     /**
      * @var integer HTTP-Status Code
-     * This value will be hold HTTP-Status Code. False if request was not successfull.
+     * This value will hold HTTP-Status Code. False if request was not successful.
      */
     public $responseCode = null;
+
+
+    /**
+     * @var array HTTP-Status Code
+     * This value will hold errors which came up while curl. Empty means no error
+     */
+    public $errors = array();
+
+
+    /**
+     * @var string User-Agent
+     * This value will be mapped to curl `CURLOPT_USERAGENT`. Default is:
+     */
+    public $userAgent = 'Yii2-Curl-Agent';
 
 
 
@@ -61,5 +82,167 @@ class Curl extends \yii\base\Widget
     public function init()
     {
         parent::init();
+    }
+
+
+    /**
+     * Start performing GET-HTTP-Request
+     *
+     * @param string  $url
+     * @param string  $body
+     * @param boolean $raw if response body contains JSON and should be decoded
+     *
+     * @return mixed response
+     */
+    public function get($url, $body = null, $raw = true)
+    {
+        return $this->httpRequest('GET', $url, $body, $raw);
+    }
+
+
+    /**
+     * Start performing HEAD-HTTP-Request
+     *
+     * @param string $url
+     * @param string $body
+     *
+     * @return mixed response
+     */
+    public function head($url, $body = null)
+    {
+        return $this->httpRequest('HEAD', $url, $body);
+    }
+
+
+    /**
+     * Start performing POST-HTTP-Request
+     *
+     * @param string  $url
+     * @param string  $body
+     * @param boolean $raw if response body contains JSON and should be decoded
+     *
+     * @return mixed response
+     */
+    public function post($url, $body = null, $raw = true)
+    {
+        return $this->httpRequest('POST', $url, $body, $raw);
+    }
+
+
+    /**
+     * Start performing PUT-HTTP-Request
+     *
+     * @param string  $url
+     * @param string  $body
+     * @param boolean $raw if response body contains JSON and should be decoded
+     *
+     * @return mixed response
+     */
+    public function put($url, $body = null, $raw = true)
+    {
+        return $this->httpRequest('PUT', $url, $body, $raw);
+    }
+
+
+    /**
+     * Start performing DELETE-HTTP-Request
+     *
+     * @param string  $url
+     * @param string  $body
+     * @param boolean $raw if response body contains JSON and should be decoded
+     *
+     * @return mixed response
+     */
+    public function delete($url, $body = null, $raw = true)
+    {
+        return $this->httpRequest('DELETE', $url, $body, $raw);
+    }
+
+
+    /**
+     * Performs HTTP request
+     *
+     * @param string  $method
+     * @param string  $url
+     * @param string  $requestBody
+     * @param boolean $raw if response body contains JSON and should be decoded
+     *
+     * @throws Exception if request failed
+     * @throws HttpException
+     *
+     * @return mixed
+     */
+    protected function httpRequest($method, $url, $requestBody = null, $raw = false)
+    {
+        //Init
+        $profile = $method.' '.$url .'#'.md5(serialize($requestBody));
+        $method = strtoupper($method);
+        $options = array();
+        $body = '';
+
+        //setup default curl options
+        $options = [
+            CURLOPT_USERAGENT      => $this->userAgent,
+            CURLOPT_RETURNTRANSFER => false,
+            CURLOPT_HEADER         => false,
+            CURLOPT_HTTPHEADER     => ['Expect:'],
+            CURLOPT_WRITEFUNCTION  => function ($curl, $data) use (&$body) {
+                $body .= $data;
+                return mb_strlen($data, '8bit');
+            },
+            CURLOPT_CUSTOMREQUEST  => $method,
+        ];
+
+        //setup connection timeout
+        if ($this->connectionTimeout !== null) {
+            $options[CURLOPT_CONNECTTIMEOUT] = $this->connectionTimeout;
+        }
+
+        //setup data parsing timeout
+        if ($this->dataTimeout !== null) {
+            $options[CURLOPT_TIMEOUT] = $this->dataTimeout;
+        }
+
+        //setup request body
+        if ($requestBody !== null) {
+            $options[CURLOPT_POSTFIELDS] = $requestBody;
+        }
+
+        //check if method is head and set no body
+        if ($method === 'HEAD') {
+            $options[CURLOPT_NOBODY] = true;
+            unset($options[CURLOPT_WRITEFUNCTION]);
+        }
+
+        //setup error reporting and profiling
+        Yii::trace("Start sending cURL-Request: $url\n" . Json::encode($requestBody), __METHOD__);
+        Yii::beginProfile($profile, __METHOD__);
+
+        //start curl
+        $curl = curl_init($url);
+        curl_setopt_array($curl, $options);
+
+        if (curl_exec($curl) === false) {
+            throw new Exception('curl request failed: ' . curl_error($curl) , curl_errno($curl));
+        }
+
+        //retrieve response code
+        $this->responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        //stop curl
+        curl_close($curl);
+        Yii::endProfile($profile, __METHOD__);
+
+        if ($this->responseCode >= 200 && $this->responseCode < 300) {
+            if ($method === 'HEAD') {
+                return true;
+            } else {
+                return $raw ? $body : Json::decode($body);
+            }
+        } elseif ($this->responseCode === 404) {
+            return false;
+        } else {
+            throw new HttpException($this->responseCode, $body);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 yii2-curl extension
 ===================
-Cool working curl extension for Yii
+Cool working curl extension for Yii, including RESTful supoort:
+
+ - POST
+ - GET
+ - HEAD
+ - PUT
+ - DELETE
 
 Installation
 ------------
@@ -25,10 +31,125 @@ or add
 "linslin/yii2-curl": "dev-master"
 ```
 
-to the require section of your `composer.json` file.
+to the require section of your `composer.json` file. Next example is a full output of a `composer.json` file including linslin/yii2-curl. This maybe will help you if you new in using composer.
+
+```
+{
+    "name": "yiisoft/yii2-app-basic",
+    "description": "Yii 2 Basic Application Template",
+    "keywords": ["yii2", "framework", "basic", "application template"],
+    "homepage": "http://www.yiiframework.com/",
+    "type": "project",
+    "license": "BSD-3-Clause",
+    "support": {
+        "issues": "https://github.com/yiisoft/yii2/issues?state=open",
+        "forum": "http://www.yiiframework.com/forum/",
+        "wiki": "http://www.yiiframework.com/wiki/",
+        "irc": "irc://irc.freenode.net/yii",
+        "source": "https://github.com/yiisoft/yii2"
+    },
+    "minimum-stability": "stable",
+    "repositories": [
+        {
+            "type": "git",
+            "url": "https://github.com/linslin/Yii2-Curl.git"
+        }
+    ],
+    "require": {
+        "php": ">=5.4.0",
+        "yiisoft/yii2": "*",
+        "yiisoft/yii2-bootstrap": "*",
+        "yiisoft/yii2-swiftmailer": "*",
+        "linslin/yii2-curl": "dev-master"
+    },
+    "require-dev": {
+        "yiisoft/yii2-codeception": "*",
+        "yiisoft/yii2-debug": "*",
+        "yiisoft/yii2-gii": "*",
+        "yiisoft/yii2-faker": "*"
+    },
+    "config": {
+        "process-timeout": 1800
+    },
+    "scripts": {
+        "post-create-project-cmd": [
+            "yii\\composer\\Installer::postCreateProject"
+        ]
+    },
+    "extra": {
+        "yii\\composer\\Installer::postCreateProject": {
+            "setPermission": [
+                {
+                    "runtime": "0777",
+                    "web/assets": "0777",
+                    "yii": "0755"
+                }
+            ],
+            "generateCookieValidationKey": [
+                "config/web.php"
+            ]
+        },
+        "asset-installer-paths": {
+            "npm-asset-library": "vendor/npm",
+            "bower-asset-library": "vendor/bower"
+        }
+    }
+}
+```
 
 
 Usage
 -----
 
-Once the extension is installed, simply use it in your code by:
+Once the extension is installed, simply use it in your code. The following example shows you how to handling a simple GET Request. 
+
+```
+<?php
+/**
+ * Yii2 test controller
+ * With RESTful support. Inspired by wenbin1989/yii2-curl.
+ *
+ * @category  Web-yii2
+ * @package   yii2-curl
+ * @author    Nils Gajsek <info@linslin.org>
+ * @copyright 2013-2014 Nils Gajsek<info@linslin.org>
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @version   1.0
+ * @link      http://www.linslin.com
+ *
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use linslin\yii2\curl;
+
+class TestController extends Controller
+{
+
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        //Init curl
+        $curl = new curl\Curl();
+
+        //get http://example.com/
+        $response = $curl->get(
+            'http://example.com/'
+        );
+    }
+}
+```
+
