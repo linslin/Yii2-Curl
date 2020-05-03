38f2c28efb4c200b7ad1c2decb262806bb6b5cea
linslin <linslin@gmx.de>
2020-05-03 13:44:34 +0200

    YII_DEBUG not defined fix for phpUnit

--- Curl.php
+++ Curl.php
@@ -631,8 +631,8 @@ class Curl
         }
 
         //setup error reporting and profiling
-        if (YII_DEBUG) {
-            Yii::trace('Start sending cURL-Request: '.$this->getUrl().'\n', __METHOD__);
+        if (defined('YII_DEBUG') && YII_DEBUG) {
+            Yii::debug('Start sending cURL-Request: '.$this->getUrl().'\n', __METHOD__);
             Yii::beginProfile($method.' '.$this->_baseUrl.'#'.md5(serialize($this->getOption(CURLOPT_POSTFIELDS))), __METHOD__);
         }
 
@@ -677,7 +677,8 @@ class Curl
         $this->_extractAdditionalCurlParameter();
 
         //end yii debug profile
-        if (YII_DEBUG) {
+        if (defined('YII_DEBUG') && YII_DEBUG) {
+            Yii::debug('End cURL-Request: '.$this->response, __METHOD__);
             Yii::endProfile($method.' '.$this->getUrl().'#'.md5(serialize($this->getOption(CURLOPT_POSTFIELDS))), __METHOD__);
         }
 
